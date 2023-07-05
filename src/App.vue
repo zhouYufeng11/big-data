c9dfc707176f1a01bc7f3c26038a0d99833fa241
SF <aroundmars@foxmail.com>
2023-07-06 00:55:01 +0800

    暂时更新为拉伸方案

--- src/App.vue
+++ src/App.vue
@@ -31,10 +31,10 @@ const getSize = () => {
 }
 const resizeWidth = () => {
   // 第一种
-  var ratio = screenParams.value.innerWidth / (root.value?.width || 2520);
-  root.value.style.transform = "scale(" + ratio + ")";
+  // var ratio = screenParams.value.innerWidth / (root.value?.width || 2520);
+  // root.value.style.transform = "scale(" + ratio + ")";
   // 第二种
-  // root.value.style.transform = `scaleY(${innerHeight / 1080}) scaleX(${innerWidth / 2520})`;
+  root.value.style.transform = `scaleY(${innerHeight / 1080}) scaleX(${innerWidth / 2520})`;
 }
 onBeforeMount(() => {
   window.onresize = () => {
